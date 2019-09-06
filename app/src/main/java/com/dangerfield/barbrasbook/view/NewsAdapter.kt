461155d8329c057464966eced9be3506b7d61e8d
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2019-09-06 16:07:40 -0500

    loads dummy data with images in recycelrview

--- app/src/main/java/com/dangerfield/barbrasbook/view/NewsAdapter.kt
+++ app/src/main/java/com/dangerfield/barbrasbook/view/NewsAdapter.kt
@@ -9,10 +9,11 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.dangerfield.barbrasbook.R
 import kotlinx.android.synthetic.main.item_article.view.*
 
-class NewsAdapter(val context: Context, list: ArrayList<String>): RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
+class NewsAdapter(private val context: Context, list: ArrayList<String>): RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
 
     var articles = ArrayList<String>()
         set(value) {
@@ -33,15 +34,19 @@ class NewsAdapter(val context: Context, list: ArrayList<String>): RecyclerView.A
             }
         }
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsAdapter.ViewHolder{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder{
         return  ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_article, parent, false))
     }
 
     override fun getItemCount() = articles.size
 
 
-    override fun onBindViewHolder(holder: NewsAdapter.ViewHolder, position: Int) {
-        holder.image.background = context.getDrawable(R.drawable.ic_launcher_background)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Glide.with(holder.image.context)
+            .load("https://www.thewrap.com/wp-content/uploads/2019/03/BarbraStreisand1.jpg")
+            .into(holder.image)
+
+        //holder.image.background = context.getDrawable(R.drawable.ic_launcher_background)
         holder.title.text = "This is a title"
         holder.preview.text = articles[position]
     }
